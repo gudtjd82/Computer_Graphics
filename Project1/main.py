22ac403024093c75835406084b727596ba24d512
gudtjd82 <gudtjd82@naver.com>
2023-04-14 05:10:20 +0900

    new orbit

--- Project1/main.py
+++ Project1/main.py
@@ -6,24 +6,33 @@ import numpy as np
 from shader import load_shaders
 from prepare_vao import *
 
-g_cam_ang = 0.
+g_cam_ang_x = 0.
+g_cam_ang_y = 0.
 g_cam_height = .1
-seq = 1    # +1 or -1   
-mouse_x_0 = 0
-mouse_y_0 = 0
-mouse_x_1 = 0
-mouse_y_1 = 0
+eye_vec = glm.vec3(0, .1, .1)
+center_vec = glm.vec3(0, 0, 0)
+up_vec = glm.vec3(0,1,0)
+view_mat = glm.lookAt(eye_vec, center_vec, up_vec)
+mouse_x = 0
+mouse_y = 0
 mouse_click = 0     # 0=>none, 1=>left, 2=>right
 mouse_press_x = 0
 mouse_press_y = 0
-# mouse_z = 0
 mouse_release_x = 0
 mouse_release_y = 0
-# mouse_click_z = 0;
-pre_mouse_diff_x = 0
-pre_mouse_diff_y = 0
-mouse_diff_x = 0
-mouse_diff_y = 0
+pre_diff_x_1 = 0
+pre_diff_y_1 = 0
+diff_x_1 = 0
+diff_y_1 = 0
+pre_diff_x_2 = 0
+pre_diff_y_2 = 0
+diff_x_2 = 0
+diff_y_2 = 0
+pre_diff_x_3 = 0
+pre_diff_y_3 = 0
+diff_x_3 = 0
+diff_y_3 = 0
+click_V = 0
 
 g_vertex_shader_src = '''
 #version 330 core
@@ -61,107 +70,107 @@ void main()
 
 #키보드 눌림 이벤트 callback 함수
 def key_callback(window, key, scancode, action, mods):
-    global g_cam_ang, g_cam_height
+    global g_cam_ang_x, g_cam_height, click_V
     if key==GLFW_KEY_ESCAPE and action==GLFW_PRESS:
         glfwSetWindowShouldClose(window, GLFW_TRUE);
     else:
         if action==GLFW_PRESS or action==GLFW_REPEAT:
             if key==GLFW_KEY_1:
-                g_cam_ang += np.radians(-10)
+                g_cam_ang_x += np.radians(-10)
             elif key==GLFW_KEY_3:
-                g_cam_ang += np.radians(10)
+                g_cam_ang_x += np.radians(10)
             elif key==GLFW_KEY_2:
                 g_cam_height += .1
             elif key==GLFW_KEY_W:
                 g_cam_height += -.1
+            elif key==GLFW_KEY_V:
+                click_V = 1;
 
 #마우스 커서 움직임 이벤트 callback 함수
 def cursor_callback(window, xpos, ypos):
-    global mouse_x_0, mouse_y_0, mouse_x_1, mouse_y_1, seq
-    # if seq == 1:
-    #     mouse_x_0 = xpos
-    #     mouse_y_0 = ypos
-    # elif seq == -1:
-    #     mouse_x_1 = xpos
-    #     mouse_y_1 = ypos
-    mouse_x_0 = xpos
-    mouse_y_0 = ypos
-#     global mouse_press_x, mouse_press_y
-    
-#     # mouse_z = zposz
-    # print("(%d, %d)" %(mouse_x, mouse_y))
+    global mouse_x, mouse_y
+    mouse_x = xpos
+    mouse_y = ypos
 
 #마우스 버튼 움직임 이벤트 callback 함수
 def button_callback(window, button, action, mod):
-    global mouse_x_0, mouse_y_0,mouse_press_x, mouse_press_y, mouse_release_x, mouse_release_y, mouse_click, mouse_diff_x, mouse_diff_y, g_cam_ang, g_cam_height, pre_mouse_diff_x, pre_mouse_diff_y
+    global mouse_x, mouse_y, mouse_press_x, mouse_press_y, mouse_release_x, mouse_release_y, mouse_click, diff_x_1, diff_y_1, g_cam_ang_x, g_cam_height, pre_diff_x_1, pre_diff_y_1
 
     #Orbit
     if button == GLFW_MOUSE_BUTTON_LEFT:
         if action == GLFW_PRESS:
-            # seq *= -1
             mouse_click = 1     
             (mouse_press_x, mouse_press_y) = glfwGetCursorPos(window)
-            print('press left btn: (%d, %d)' % (mouse_press_x, mouse_press_y))
+            # print('press left btn: (%d, %d)' % (mouse_press_x, mouse_press_y))
         elif action == GLFW_RELEASE:
             mouse_click = 0
-            pre_mouse_diff_x = mouse_diff_x
-            pre_mouse_diff_y = mouse_diff_y
-            # init_mouse()
-            # (mouse_release_x, mouse_release_y) = glfwGetCursorPos(window)
-            # print('press left btn: (%d, %d)' % (mouse_x_0, mouse_y_0))
-            # print('release left btn: (%d, %d)' % (mouse_release_x, mouse_release_y))
-            # mouse_diff_x = mouse_x_0 - mouse_release_x
-            # mouse_diff_y = mouse_y_0 - mouse_release_y
-            # print('difference left btn: (%d, %d)' % (mouse_diff_x, mouse_diff_y))
-            # g_cam_ang += np.radians(mouse_diff_x/10)
-            # g_cam_height += mouse_diff_y/100
+            pre_diff_x_1 = diff_x_1
+            pre_diff_y_1 = diff_y_1
+
     #Pan
     elif button == GLFW_MOUSE_BUTTON_RIGHT:
         if action == GLFW_PRESS:
             mouse_click = 2
-            (mouse_x_0, mouse_y_0) = glfwGetCursorPos(window)
-            seq = 1
-            print('press left btn: (%d, %d)' % (mouse_x_0, mouse_y_0))
+            (mouse_press_x, mouse_press_y) = glfwGetCursorPos(window)
+            # print('press left btn: (%d, %d)' % (mouse_x, mouse_y))
         elif action == GLFW_RELEASE:
             mouse_click = 0
-            (mouse_release_x, mouse_release_y) = glfwGetCursorPos(window)
-            seq = 0
-            init_mouse()
-            print('release left btn: (%d, %d)' % (mouse_release_x, mouse_release_y))
+            pre_diff_x_1 = diff_x_1
+            pre_diff_y_1 = diff_y_1
+            # (mouse_release_x, mouse_release_y) = glfwGetCursorPos(window)
+            # print('release left btn: (%d, %d)' % (mouse_release_x, mouse_release_y))
             
     else:
         mouse_click = 0
 
 #마우스 스크롤 움직임 이벤트 callback 함수
 def scroll_callback(window, xoffset, yoffset):
+    global mouse_click, eye_vec
+    mouse_click = 3
     print('mouse wheel scroll: %d, %d' % (xoffset, yoffset))
 
-def init_mouse():
-    global mouse_x_0, mouse_x_1, mouse_y_0, mouse_y_1
-    mouse_x_0 = 0
-    mouse_x_1 = 0
-    mouse_y_0 = 0
-    mouse_y_1 = 0
-
 def cam_orbit():
-    global mouse_diff_x, mouse_diff_y, g_cam_ang, g_cam_height, seq
+    global diff_x_1, diff_y_1, g_cam_ang_x, g_cam_ang_y
     
-    # print('mouse position: (%d, %d)' % (mouse_x, mouse_y))
-    #azimuth
-    mouse_diff_x = pre_mouse_diff_x + mouse_press_x - mouse_x_0
-    g_cam_ang = np.radians(mouse_diff_x/10)
+    diff_x_1 = pre_diff_x_1 + mouse_press_x - mouse_x
+    diff_y_1 = pre_diff_y_1 + mouse_press_y - mouse_y
+    g_cam_ang_x = -np.radians(diff_x_1) * 10
+    g_cam_ang_y = -np.radians(diff_y_1) * 10
+    # update_view_mat()
 
-    #elevation
-    mouse_diff_y = pre_mouse_diff_y + mouse_press_y - mouse_y_0
-    g_cam_height = 0.1 + -mouse_diff_y/1000
-    print('difference left btn: (%d, %d)' % (mouse_diff_x, mouse_diff_y))
+    # diff_x_1 = mouse_press_x - mouse_x
+    # diff_y_1 = mouse_press_y - mouse_y
+    # if g_cam_ang_y > 89:
+    #     g_cam_ang_y = 89
+    # elif g_cam_ang_y < -89:
+    #     g_cam_ang_y = -89
+
     
+    # g_cam_height = 0.1 + -diff_y_1/1000
+    # print('difference left btn: (%d, %d)' % (diff_x_1, diff_y_1))
  
 def cam_pan():
-    global mouse_diff_x, mouse_diff_y, g_cam_ang, g_cam_height
+    global diff_x_2, diff_y_2, g_cam_ang_x, g_cam_height
+
+    diff_x_2 = pre_diff_x_2 + mouse_press_x - mouse_x
+    diff_y_2 = pre_diff_y_2 + mouse_press_y - mouse_y
+    
+
+def cam_zoom():
+    global diff_x_3, diff_y_3
+
+def update_view_mat():
+    global view_mat
+    R = glm.mat4(1.0)
+    R = glm.rotate(R, glm.radians(g_cam_ang_y), glm.vec3(1,0,0))
+    R = glm.rotate(R, glm.radians(g_cam_ang_x), glm.vec3(0,1,0))
+    # eye_vec = glm.rotate(eye_vec, g_cam_ang_y, glm.vec3(1,0,0))
+    # eye_vec = glm.rotate(eye_vec, g_cam_ang_x, glm.vec3(0,1,0))
+    # view_mat = glm.lookAt(glm.vec3(0 , 0.1, 0.1), glm.vec3(0,0,0), glm.vec3(0,1,0))
+    view_mat = glm.lookAt(eye_vec, center_vec, up_vec) * R
 
 def main():
-    global mouse_click, seq, seq
+    global click_V, view_mat, eye_vec, center_vec, up_vec
     # initialize glfw
     if not glfwInit():
         return
@@ -192,7 +201,7 @@ def main():
     # prepare vaos
     vao_triangle = prepare_vao_triangle()
     vao_frame = prepare_vao_frame()
-
+    V = glm.lookAt(glm.vec3(0, .1, .1), glm.vec3(0,0,0), glm.vec3(0,1,0))
     # loop until the user closes the window
     while not glfwWindowShouldClose(window):
         # render
@@ -207,21 +216,43 @@ def main():
         # use orthogonal projection (we'll see details later)
         P = glm.ortho(-1,1,-1,1,-1,1)
 
-        # view matrix
-        # rotate camera position with g_cam_ang / move camera up & down with g_cam_height
+        
+
+        #왼쪽 클릭 시에는(1) orbit, 오른쪽 클릭 시에는(2) pan, 휠 회전 시에는(3) zoom
         if mouse_click == 1:
             cam_orbit()
-            # print('difference left btn: (%d, %d)' % (mouse_diff_x, mouse_diff_y))
+            update_view_mat()
+            # eye_vec = glm.rotate(eye_vec, g_cam_ang_y, glm.vec3(1,0,0))
+            # eye_vec = glm.rotate(eye_vec, g_cam_ang_x, glm.vec3(0,1,0))
+            # eye_vec = R * eye_vec 
+            # eye_vec = glm.vec3(.1*np.sin(g_cam_ang_x),g_cam_height,.1*np.cos(g_cam_ang_x))
+            # eye_vec = glm.rotate(eye_vec, glm.radians(90), glm.vec3(1,0,0))
+            # eye_vec = glm.rotate(eye_vec, g_cam_ang_y, glm.vec3(0,0,0))
+            # V = glm.lookAt(eye_vec, center_vec, up_vec)
+            # print('difference left btn: (%d, %d)' % (diff_x_1, diff_y_1))
         elif mouse_click == 2:
             cam_pan()
-            # print('difference right btn: (%d, %d)' % (mouse_diff_x, mouse_diff_y))
+            change_x = diff_x_2/1000
+            change_y = diff_y_2/1000
+            eye_vec = glm.vec3(.1*np.sin(g_cam_ang_x) + change_x, g_cam_height + change_y,.1*np.cos(g_cam_ang_x) + change_x)
+            center_vec = glm.vec3(change_x, change_y, change_x)
+            V =glm.lookAt(eye_vec, center_vec, up_vec)
+        
+        V = view_mat
+        # elif mouse_click == 3:
+        # update_view_mat()   
+        # V = glm.lookAt(eye_vec, center_vec, up_vec)
+        
+            
 
-        up_vec = glm.vec3(0,1,0)
-        center_vec = glm.vec3(0, 0, 0)
+            # V = glm.lookAt(eye_vec, center_vec, up_vec)
+            # print('difference right btn: (%d, %d)' % (diff_x_1, diff_y_1))
 
-        V = glm.lookAt(glm.vec3(.1*np.sin(g_cam_ang),g_cam_height,.1*np.cos(g_cam_ang)), center_vec, up_vec)
-        # V = glm.lookAt(glm.vec3(0.1 , 0.1, 0.1), glm.vec3(0,0,0), glm.vec3(0,1,0))
-            # print('press left btn: (%d, %d)' % (mouse_x_0, mouse_press_y))
+
+        # view matrix
+        # rotate camera position with g_cam_ang_x / move camera up & down with g_cam_height
+        
+            # print('press left btn: (%d, %d)' % (mouse_x, mouse_press_y))
             # print('release left btn: (%d, %d)' % (mouse_release_x, mouse_release_y))
             
 
@@ -260,6 +291,9 @@ def main():
 
         # current frame: P*V*M
         MVP = P*V*M
+        if click_V == 1:
+            print(V)
+            click_V = 0
         glUniformMatrix4fv(MVP_loc, 1, GL_FALSE, glm.value_ptr(MVP))
 
         # draw triangle w.r.t. the current frame
